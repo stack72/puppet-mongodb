955751d1e9cc912f715f875ca8a8ce56aec14050
stack72 <public@paulstack.co.uk>
2014-02-18 10:09:44 +0000

    Downgrading to mongodb 2.4.7 to try and get around the bad file descriptors

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
-  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.9.tar.gz'
-  sha1 '3aa495cf32769a09ee9532827391892d96337d6b'
-  version '2.4.9-boxen1'
+  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.7.tar.gz'
+  sha1 'abef63992fe12e4e68a7d9de01d8d8eaa8705c9a'
+  version '2.4.7-boxen1'
 
   skip_clean :all
 
