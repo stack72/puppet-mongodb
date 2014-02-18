f9d60f645e72595bcf0328767880dbbdc5e66e68
stack72 <public@paulstack.co.uk>
2014-02-18 10:24:39 +0000

    Bumping back to 2.4.9 with a different sha1

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
-  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.7.tar.gz'
-  sha1 'abef63992fe12e4e68a7d9de01d8d8eaa8705c9a'
-  version '2.4.7-boxen1'
+  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.9.tar.gz'
+  sha1 '04d49071102d86ac06f35ed9e4c855a677d97c68'
+  version '2.4.9-boxen1'
 
   skip_clean :all
 
