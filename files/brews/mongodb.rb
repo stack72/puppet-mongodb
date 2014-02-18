c0f3756f36728dc80a23c3ae3e8c6932917f9695
stack72 <public@paulstack.co.uk>
2014-02-18 10:27:11 +0000

    Adding a different sha1 for mongo 2.4.9

--- files/brews/mongodb.rb
+++ files/brews/mongodb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
   url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.9.tar.gz'
-  sha1 '04d49071102d86ac06f35ed9e4c855a677d97c68'
+  sha1 '3aa495cf32769a09ee9532827391892d96337d6b'
   version '2.4.9-boxen1'
 
   skip_clean :all
